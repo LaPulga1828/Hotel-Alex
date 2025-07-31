414420f0efa4b6e587cf8d3e4bb328e8815b31e4
GrisMichi <leidymariamotr@gmail.com>
2025-07-31 13:47:19 -0500

    no se

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,36 @@
 <template>
   <div>
     <q-layout view="hHh lpR fff">
+        <q-header elevated class="bg-primary text-white" height-hint="98" :style="{ color: 'black' }">
+          <q-toolbar>
+            <q-toolbar-title class=" text-dark">
+              <q-avatar>
+                <img src="./assets/img/logo.jpeg" />
+              </q-avatar >
+              Hotel Portal Guane
+            </q-toolbar-title>
+          </q-toolbar>
 
-      <q-header elevated class="bg-primary text-white">
-        <q-toolbar>
-          <q-toolbar-title>
-            <q-avatar>
-              <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-            </q-avatar>
-            Title
-          </q-toolbar-title>
+          <q-tabs align="left" class=" text-dark">
+            <q-route-tab to="/page1" label="Inicio" />
+            <q-route-tab to="/page2" label="Habitaciones" />
+            <q-route-tab to="/page3" label="Deportes extremos" />
+            <q-route-tab to="/page1" label="Nuestra gastronomía" />
+            <q-route-tab to="/page2" label="Servicios extra" />
+          </q-tabs>
+        </q-header>
 
-          <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
-        </q-toolbar>
-      </q-header>
-
-      <q-drawer style="display: flex; flex-direction: column; gap: 5px;" v-model="rightDrawerOpen" side="right" overlay
-        behavior="mobile">
+      <q-drawer
+        style="display: flex; flex-direction: column; gap: 5px"
+        v-model="rightDrawerOpen"
+        side="right"
+        overlay
+        behavior="mobile"
+      >
         <!-- drawer content -->
-        <q-btn  label="Ventas"  glossy color="purple" to="/ventas"/>
-        <q-btn  label="Compras"  glossy color="purple" to="/compras"/>
-        <q-btn  label="Productos" glossy color="purple" to="/productos" />
-        
+        <q-btn label="Ventas" glossy color="purple" to="/ventas" />
+        <q-btn label="Compras" glossy color="purple" to="/compras" />
+        <q-btn label="Productos" glossy color="purple" to="/productos" />
       </q-drawer>
 
       <q-page-container>
@@ -32,21 +41,20 @@
         <q-toolbar>
           <q-toolbar-title>
             <q-avatar>
-              <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+              <img src="./assets/img/logo.jpeg" />
             </q-avatar>
-            <div>Title</div>
+            <div>t</div>
           </q-toolbar-title>
         </q-toolbar>
       </q-footer>
-
     </q-layout>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const rightDrawerOpen = ref(false)
+import { ref } from "vue";
+const rightDrawerOpen = ref(false);
 function toggleRightDrawer() {
-  rightDrawerOpen.value = !rightDrawerOpen.value
+  rightDrawerOpen.value = !rightDrawerOpen.value;
 }
 </script>
